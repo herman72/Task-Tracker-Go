4cbbed6ae674f20d780d70d94cfb14e05a0492ee
herman72 <m.sherafati7@gmail.com>
2024-09-14 06:10:33 +0300

    feat: add task and save tasks

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"time"
 )
 
 const taskFile = "task.json"
@@ -24,8 +25,52 @@ func initTaskFile() {
 	}
 }
 
+func loadTask() []Task {
+	file, _ := os.ReadFile(taskFile)
+	var task []Task
+	_ = json.Unmarshal(file, &task)
+	return task
+}
+
+func saveTasks(tasks []Task) {
+	file, _ := json.MarshalIndent(tasks, "", " ")
+	_ = os.WriteFile(taskFile, file, 0644)
+}
+
+func addTask(description string) {
+	tasks := loadTask()
+	newTask := Task{
+		ID:          len(tasks) + 1,
+		Description: description,
+		Status:      "todo",
+		CreatedAt:   time.Now().Format(time.RFC3339),
+		UpdatedAt:   time.Now().Format(time.RFC3339),
+	}
+	tasks = append(tasks, newTask)
+	saveTasks(tasks)
+	fmt.Printf("Task added successfully (ID: %d)\n", newTask.ID)
+}
+
 func main() {
 	initTaskFile()
+
+	if len(os.Args) < 2 {
+		fmt.Println("Expected 'add' command with task description")
+		return
+	}
+	
+	command := os.Args[1]
+
+	switch command {
+	case "add":
+		if len(os.Args) < 3 {
+            fmt.Println("Please provide a task description")
+        } else {
+            addTask(os.Args[2])
+        }
+	default:
+		fmt.Println("Unknown command:", command)
+	}
 	fmt.Println("Task Tracker CLI")
 
 }
