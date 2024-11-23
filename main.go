32e924629f4feb809c0dae2da27810586a4c5675
herman72 <m.sherafati7@gmail.com>
2024-11-23 02:51:43 +0200

    feat: add switch case

--- main.go
+++ main.go
@@ -2,183 +2,132 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"flag"
 	"fmt"
-	// "go/scanner"
 	"os"
-	// "strconv"
-	"time"
 )
 
-const taskFile = "task.json"
 
-type Task struct {
-	ID          int    `json:"id"`
-	Description string `json:"description"`
-	Status      string `json:"status"`
-	CreatedAt   string `json:"createdAt"`
-	UpdatedAt   string `json:"updatedAt"`
+type User struct {
+	ID int
+	email string
+	password string
 }
 
-func initTaskFile() {
-	if _, err := os.Stat(taskFile); os.IsNotExist(err) {
-		emptyTask := []Task{}
-		file, _ := json.MarshalIndent(emptyTask, "", " ")
-		_ = os.WriteFile(taskFile, file, 0644)
-	}
-}
-
-func loadTask() []Task {
-	file, _ := os.ReadFile(taskFile)
-	var task []Task
-	_ = json.Unmarshal(file, &task)
-	return task
-}
-
-func saveTasks(tasks []Task) {
-	file, _ := json.MarshalIndent(tasks, "", " ")
-	_ = os.WriteFile(taskFile, file, 0644)
-}
-
-func addTask(description string) {
-	tasks := loadTask()
-	newTask := Task{
-		ID:          len(tasks) + 1,
-		Description: description,
-		Status:      "todo",
-		CreatedAt:   time.Now().Format(time.RFC3339),
-		UpdatedAt:   time.Now().Format(time.RFC3339),
-	}
-	tasks = append(tasks, newTask)
-	saveTasks(tasks)
-	fmt.Printf("Task added successfully (ID: %d)\n", newTask.ID)
-}
-
-func updateTask(id int, description string) {
-	tasks := loadTask()
-	for i := range tasks {
-		if tasks[i].ID == id {
-			tasks[i].Description = description
-			tasks[i].UpdatedAt = time.Now().Format(time.RFC3339)
-			saveTasks(tasks)
-			fmt.Printf("Task updated successfully (ID: %d)\n", id)
-
-		}
-	}
-
-}
-
-func deleteTask(id int) {
-	tasks := loadTask()
-	found := false
-	for i := range tasks {
-		if tasks[i].ID == id {
-			tasks = append(tasks[:i], tasks[i+1:]...)
-			saveTasks(tasks)
-			fmt.Printf("Task deleted successfully (ID: %d)\n", id)
-			found = true
-			break
-
-		}
-	}
-	if !found {
-		fmt.Printf("Task not found (ID: %d)\n", id)
-	}
-}
-
-func markTask(id int, status string) {
-	tasks := loadTask()
-	for i := range tasks {
-		if tasks[i].ID == id {
-			tasks[i].Status = status
-			tasks[i].UpdatedAt = time.Now().Format(time.RFC3339)
-			saveTasks(tasks)
-			fmt.Printf("Task status with Id %d updated", id)
-			break
-		} else {
-			fmt.Printf("Task with ID %d was not found", id)
-		} 
-	}
-}
+var userStorage []User
 
 func main() {
 	fmt.Println("Hello todo application")
 	command := flag.String("command", "no command provided", "add, update, delete, mark-done, mark-in-progress")
 	flag.Parse()
-	scanner := bufio.NewScanner(os.Stdin)
 
-	if * command == "create-task" {
-		var name, dudate, category string
-		fmt.Printf("pls enter the task title: ")
-
+	for {
+		runCommand(*command)
+		fmt.Println("Enter command: ")
+		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
-		name = scanner.Text()
-
-		fmt.Printf("pls enter the task dudate: ")
-		scanner.Scan()
-		dudate = scanner.Text()
-
-		fmt.Printf("pls enter the task category: ")
-		scanner.Scan()
-		category = scanner.Text()
-
-		fmt.Printf("Task title: %s, Task dudate: %s, Task category: %s", name, dudate, category)
+		scanner.Text()
 	}
 
-	// initTaskFile()
+	
+	fmt.Printf("userStorage %+v\n",userStorage)
 
-	// if len(os.Args) < 2 {
-	// 	fmt.Println("Expected 'add' command with task description")
-	// 	return
-	// }
+}
 
-	// command := os.Args[1]
+func runCommand(command string) {
 
-	// switch command {
-	// case "add":
-	// 	if len(os.Args) < 3 {
-	// 		fmt.Println("Please provide a task description")
-	// 	} else {
-	// 		addTask(os.Args[2])
-	// 	}
-	// case "update":
-	// 	if len(os.Args) < 3 {
-	// 		fmt.Println("Please provide a task description")
-	// 	} else {
-	// 		idStr := os.Args[2]
-	// 		id, _ := strconv.Atoi(idStr)
-	// 		description := os.Args[3]
-	// 		updateTask(id, description)
-	// 	}
-	// case "delete":
-	// 	if len(os.Args) < 2 {
-	// 		fmt.Printf("please provide the task ID")
-	// 	} else {
-	// 		idStr := os.Args[2]
-	// 		id, _ := strconv.Atoi(idStr)
-	// 		deleteTask(id)
-	// 	}
-	// case "mark-done":
-	// 	if len(os.Args) < 2 {
-	// 		fmt.Print("Please provide the task ID")
-	// 	} else {
-	// 		idStr := os.Args[2]		
-	// 		id, _ := strconv.Atoi(idStr)
-	// 		markTask(id, "done")
-	// 	}
-	// case "mark-in-progress":
-	// 	if len(os.Args) < 2 {
-	// 		fmt.Print("Please provide the task ID")
-	// 	} else {
-	// 		idStr := os.Args[2]		
-	// 		id, _ := strconv.Atoi(idStr)
-	// 		markTask(id, "in-progress")
-	// 	}
+	switch command {
+	case "create-task":
+		createTask()
+	case "create-category":
+		createCategory()
+	case "register-user":
+		registerUser()
+	case "login":
+		login()
+	case "exit":
+		os.Exit(0)
+	default:
+		fmt.Println("command is not valid", command)
+	}
 
-	// default:
-	// 	fmt.Println("Unknown command:", command)
-	// }
-	// fmt.Println("Task Tracker CLI")
+}
 
+func createTask() {
+	
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var name, duedate, category string
+		
+	fmt.Println("pls enter the task title: ")
+	scanner.Scan()
+	name = scanner.Text()
+
+	fmt.Println("pls enter the task duedate: ")
+	scanner.Scan()
+	duedate = scanner.Text()
+
+	fmt.Println("pls enter the task category: ")
+	scanner.Scan()
+	category = scanner.Text()
+
+	fmt.Println("Task title: , Task duedate: , Task category: ", name, duedate, category)
+
+}
+
+func registerUser(){
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var id, email, password string
+	fmt.Println("pls enter the User email: ")
+	scanner.Scan()
+	email = scanner.Text()
+
+	fmt.Println("pls enter the User password: ")
+	scanner.Scan()
+	password = scanner.Text()
+
+	id = email
+
+	fmt.Println("User name:, User email:, User password:", id, email, password)
+
+	user := User{
+		ID: len(userStorage) + 1,
+		email: email,
+		password: password,
+	}
+
+	userStorage = append(userStorage, user)
+}
+
+func createCategory(){
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var title, color string
+
+	fmt.Println("pls enter the Category title: ")
+	scanner.Scan()
+	title = scanner.Text()
+
+	fmt.Println("pls enter the Category color: ")
+	scanner.Scan()
+	color = scanner.Text()
+
+	fmt.Println("Category title:, Category color:", title, color)
+}
+
+func login(){
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var email, password string
+	
+	fmt.Println("pls enter the User email: ")
+	scanner.Scan()
+	email = scanner.Text()
+
+	fmt.Println("pls enter the User password: ")
+	scanner.Scan()
+	password = scanner.Text()
+
+	fmt.Println("User email:, User password", email, password)
 }
